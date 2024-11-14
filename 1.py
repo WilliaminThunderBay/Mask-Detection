de9a54ff97041b512b8fea53a2efdc55665f2d6b
WilliaminThunderBay <jhu29@lakeheadu.ca>
2024-11-14 16:10:48 -0500

    Update 1.py

--- 1.py
+++ 1.py
@@ -130,8 +130,8 @@ elif selected == "Image Mask Detection":
             color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
             label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
 
-            cv2.putText(image, label, (startX, startY - 20), cv2.FONT_HERSHEY_SIMPLEX, 1.5, color, 8)
-            cv2.rectangle(image, (startX, startY), (endX, endY), color, 8)
+            cv2.putText(image, label, (startX, startY - 20), cv2.FONT_HERSHEY_SIMPLEX, 2, color, 10)
+            cv2.rectangle(image, (startX, startY), (endX, endY), color, 10)
 
         with col2:
             st.image(image[:, :, ::-1], channels="RGB", caption="Prediction Image", use_column_width=True)
@@ -154,8 +154,8 @@ elif selected == "Real-time Camera Detection":
                 color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
                 label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
 
-                cv2.putText(image, label, (startX, startY - 20), cv2.FONT_HERSHEY_SIMPLEX, 2.0, color, 8)
-                cv2.rectangle(image, (startX, startY), (endX, endY), color, 8)
+                cv2.putText(image, label, (startX, startY - 20), cv2.FONT_HERSHEY_SIMPLEX, 2.0, color, 10)
+                cv2.rectangle(image, (startX, startY), (endX, endY), color, 8-10)
 
             return image
 
